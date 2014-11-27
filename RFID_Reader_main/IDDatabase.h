4cd7929d9984677a55352737e75c2287e805cd12
Watson Tungjunyatham <starrodkirby86@mail.fresnostate.edu>
2014-11-26 21:26:40 -0800

    IDDatabase.h
    Added the basics of the ID class, which will be used for the RFID
    Reader. This is the framework.

---
+++ RFID_Reader_main/IDDatabase.h
@@ -0,0 +1,137 @@
+/*
+    IDDatabase.h
+    
+    This class is the basic framework of the RFID cards. You can manipulate various RFID-related stuff with this class...
+    
+    Version:
+        0.dongerz
+    Authors:
+        Watson Tungjunyatham
+        
+*/
+
+#ifndef _IDDATABASE_H_
+#define _IDDATABASE_H_
+
+#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
+
+class ID
+{
+    // This is the class for an individual ID.
+    
+    // ==============================================
+    // PROPERTIES
+    // ============================================== 
+    
+        private:
+            String IDNumber;    // The ID Number.           e.g. 06008F6747
+            String IDOwner;     // The Owner of the ID.     e.g. Kim Jong Un
+       
+    // ==============================================
+    // CLASS CONSTRUCTORS
+    // ==============================================
+        
+        ID(String arg_IDNumber, String arg_IDOwner)
+        {
+            // Constructor for ID class when both
+            // arguments are given. (Ideally)
+            IDNumber = arg_IDNumber; 
+            IDOwner  =  arg_IDOwner;
+        }
+        
+        ID(String arg_IDNumber)
+        {
+            // Constructor for ID class when
+            // ONLY ID number is given.
+            IDNumber = arg_IDNumber;
+            IDOwner  = "Unknown";
+        }
+        
+        ID()
+        {
+            // Constructor for no arguments
+            // You shouldn't really stumble upon this...
+            IDNumber = "1234567890";
+            IDOwner  = "Foo Fighter";
+        }    
+
+    // ==============================================
+    // FUNCTIONS
+    // ============================================== 
+    
+        public:
+    
+    // ==============================================
+    // GET FUNCTIONS
+    // ==============================================
+    
+        String getID()      { return IDNumber; }
+        String getOwner()   { return IDOwner; }
+
+    // ==============================================
+    // OTHER FUNCTIONS
+    // ==============================================
+    
+        // Boolean checks
+        bool isIDBlank()    { return (IDNumber == NULL); }
+        bool isOwnerBlank() { return (IDOwner == NULL);  }
+        
+        void printID(HardwareSerial *serIn)
+        {
+            // Display ID and owner in a simple Serial print.
+            // If the components are blank, display an error
+            // message instead.
+            
+            // Uses the Serial, so the Serial must be
+            // communicated with this function.
+            HardwareSerial *_Serial;
+            _Serial = serIn;
+            
+            if((isIDBlank()) && (isOwnerBlank()))
+            {
+                _Serial->print("ID: ");
+                _Serial->println(IDNumber);
+                _Serial->print("OWNER: ");
+                _Serial->println(IDOwner);
+            }
+            else
+            {
+                _Serial->println("ERROR: Null ID or Owner.");
+            }
+        }
+        
+        ~ID();
+    
+};
+
+class IDDatabase
+{
+    // This is the class for a database of IDs.
+    
+    // ==============================================
+    // PROPERTIES
+    // ============================================== 
+    
+    private:
+        vector<ID> database;
+        HardwareSerial *_Serial;
+    
+    // ==============================================
+    // CLASS CONSTRUCTORS
+    // ==============================================
+        IDDatabase(HardwareSerial *serIn)
+        {
+            // Constructor a database.
+            // Initially starts empty, with only a Serial
+            // to communicate with.
+            _Serial = serIn;
+        }
+    // ==============================================
+    // FUNCTIONS
+    // ==============================================     
+    public:
+    
+}
+
+#endif
---
+++ RFID_Reader_main/RFID_Reader_main.ino
@@ -0,0 +1,36 @@
+#include <IDDatabase.h>
+
+/*  
+  IEEE_RFID_Reader
+  
+  ID Card scanning system intended for system logins.
+  
+  Version:
+    0.01
+  
+  Authors:
+    - Watson Tungjunyatham
+    - Jaehoon Hwang
+    - Conor Swezey
+    - Daniel Irvin
+    
+  For all changes, details, bugs, grumbles, and everything else,
+  please check out our very own (and shiny!) Github.
+  
+  https://github.com/starrodkirby86/IEEE_RFID_Reader
+  
+  We want YOU to help! Unless you're a Somalian pirate. Just
+  kidding, we're an Equal Opportunities employer.
+  
+*/
+
+#include <SoftwareSerial.h>
+#include "IDDatabase.h"
+
+void setup()
+{
+}
+
+void loop()
+{
+}
